d415dd309bec544403a52212650ac9a4301e8345
Tomo Krajina <tkrajina@gmail.com>
2016-11-15 06:48:28 +0100

    Haversine and nonhaversine test

--- src/test/java/info/puzz/calories/LocationUtilsTest.java
+++ src/test/java/info/puzz/calories/LocationUtilsTest.java
@@ -7,7 +7,14 @@ public class LocationUtilsTest {
     @Test
     public void testDistance() {
         Location loc1 = new Location(0, 0, null, null, null);
-        Location loc2 = new Location(0, 1, null, null, null);
-        Assert.assertTrue(Math.abs(LocationUtils.ONE_DEGREE - loc1.distance(loc2)) < 100);
+        Location loc2 = new Location(1 / 1000., 0, null, null, null);
+        Assert.assertTrue(Math.abs(LocationUtils.ONE_DEGREE / 1000. - loc1.distance(loc2)) < 100 / 1000.);
+    }
+
+    @Test
+    public void testLongDistance() {
+        Location loc1 = new Location(0, 0, null, null, null);
+        Location loc2 = new Location(0, 45, null, null, null);
+        Assert.assertTrue(Math.abs(LocationUtils.ONE_DEGREE * 45 - loc1.distance(loc2)) < 45 * 100);
     }
 }
